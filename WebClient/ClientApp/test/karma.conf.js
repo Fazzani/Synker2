91a066773ade6c403030c03f404ffeb2535d06c5
Fazzani <tunisienheni@outlook.com>
2017-10-08 16:17:33 +0200

    HFA: Fix karma tests

--- WebClient/ClientApp/test/karma.conf.js
+++ WebClient/ClientApp/test/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
         autoWatch: true,
         browsers: ['Chrome'],
         mime: { 'application/javascript': ['ts','tsx'] },
-        singleRun: true,
+        singleRun: config.singleRun || false,
         webpack: require('../../webpack.config.js')().filter(config => config.target !== 'node'), // Test against client bundle, because tests run in a browser
         webpackMiddleware: { stats: 'errors-only' }
     });
