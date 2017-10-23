746933d7297214b6590f6aa7f651fbae5a242e6d
Fazzani <tunisienheni@outlook.com>
2017-10-24 01:02:55 +0200

    [skip ci] fix bach

--- fetchNewCommands.sh
+++ fetchNewCommands.sh
@@ -11,11 +11,15 @@ hostApi='http://0.0.0.0:56800/api/v1'
 echo  $hostApi
 
 echo "Getting auth token.."
-curl --insecure -o bearer.json -X POST -H "Content-type: application/json" -d '{"password": "batch2017", "username": "support@synker.ovh"}' $hostApi/auth/token > bearer.json
-bearer=($(cat bearer.json | jq '.accessToken'))
+curl --insecure -o bearer.json -X POST -H "Content-type: application/json"  -d '{"password": "batch2017", "username": "support@synker.ovh"}' $hostApi/auth/token
+bearer=($(cat bearer.json | jq -r '.accessToken'))
 #echo "Put token to the file $bearer bearer.json"
 echo "Get commands"
-curl -v --insecure -o commands.json -X GET -H "Content-type:application/json" $hostApi/command?all=true
-v1=($(cat commands.json | jq '.'))
-echo $v1
+curl -v --insecure -o commands.json -X GET -H "Content-type:application/json" -H "Authorization:Bearer $bearer"  $hostApi/command/users/1?all=true
+#cat commands.json
+v1=($(jq -rc '.[]."commandText"' commands.json))
+#echo $v1
+#printf '%s\n' "${v1[@]}"
+cat commands.json | jq -r '.[].commandText'
+#TODO: Mettre la liste des commandes récupérer dans un array dans le bon ordre et les exécuter 
 quit 0;
