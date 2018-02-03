19de23d0fc4c74cb47c2499d7dd12498d6e94acd
Fazzani <tunisienheni@outlook.com>
2018-02-03 15:25:12 +0100

    buildbroker: fix logger

---
+++ hfa.Notification.Brokers/Common.cs
@@ -0,0 +1,23 @@
+﻿using Hfa.SyncLibrary;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hfa.Notification.Brokers
+{
+    public class Common
+    {
+        public static ILogger Logger(string cat = "default") => _LoggerFactory.CreateLogger(cat);
+        private static ILoggerFactory _LoggerFactory = null;
+
+        static Common()
+        {
+            _LoggerFactory = (ILoggerFactory)Init.ServiceProvider.GetService(typeof(ILoggerFactory));
+        }
+
+    }
+}
--- hfa.Notification.Brokers/Program.cs
+++ hfa.Notification.Brokers/Program.cs
@@ -10,23 +10,25 @@ using System;
 using System.Text;
 using hfa.Notification.Brokers.Emailing;
 using System.Threading;
+using Microsoft.Extensions.Logging;
 
 namespace hfa.Notification.Brokers
 {
     public class Program
     {
         private const string MailQueueName = "synker.mail.queue";
-
+        private static ILogger _logger;
         private static IOptions<RabbitMQConfiguration> _rabbitConfig;
         private static INotificationService _notifService;
 
         static void Main(string[] args)
         {
             Init.Build();
+            _logger = Common.Logger(nameof(Program));
             _rabbitConfig = Init.ServiceProvider.GetService<IOptions<RabbitMQConfiguration>>();
             _notifService = Init.ServiceProvider.GetService<INotificationService>();
 
-            Console.WriteLine("starting consumption");
+            _logger.LogInformation("starting consumption");
             var factory = new ConnectionFactory()
             {
                 HostName = _rabbitConfig.Value.Hostname,
@@ -53,20 +55,22 @@ namespace hfa.Notification.Brokers
                         var message = Encoding.UTF8.GetString(body);
                         var mail = JsonConvert.DeserializeObject<EmailNotification>(message);
                         _notifService.SendMailAsync(mail, CancellationToken.None).GetAwaiter().GetResult();
-                        Console.WriteLine($"Mail from {mail.From} to {mail.To}");
+                        _logger.LogInformation($"Mail from {mail.From} to {mail.To}");
                     };
 
                     channel.BasicConsume(queue: MailQueueName,
                                             autoAck: true,
                                             consumer: consumer);
 
+                    do
+                    {
+                        Thread.Sleep(200);
+                    } while (true);
+
                 }
             }
 
-            do
-            {
-                Thread.Sleep(200);
-            } while (true);
+            
         }
     }
 }
