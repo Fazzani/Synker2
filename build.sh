cdb5902e0863116fe88a389077d26a0069d3cb6e
Fazzani <tunisienheni@outlook.com>
2018-02-11 17:27:51 +0100

    docker build Travis

--- build.sh
+++ build.sh
@@ -4,12 +4,22 @@ set -evuxo
 
 artifactsFolder="./artifacts"
 
-if [ $DOCKER_BUILD="true" ]; then
-  echo "Is Docker build";
-fi
-
 if [ -d $artifactsFolder ]; then  
   rm -R $artifactsFolder
+fi
+
+dockerImageBase="synker/broker"
+dockerImage=$dockerImageBase:$TRAVIS_BUILD_NUMBER-$arch
+echo $dockerImage
+
+if [ $DOCKER_BUILD="true" ]; then
+  echo "Is Docker build";
+  docker build --build-arg arch=$arch -t $dockerImage -t $dockerImageBase:latest .
+  docker images
+  docker run --rm $dockerImage
+  docker login -u=$DOCKER_USER -p=$DOCKER_PASS
+  docker push $dockerImage
+  exit 0
 fi
 
 cd WebClient
