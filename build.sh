19e777c8d2b85ce9ce46b5aa9e03c8cd83629006
Fazzani <tunisienheni@outlook.com>
2018-02-11 16:17:21 +0100

    fixs

--- build.sh
+++ build.sh
@@ -11,4 +11,10 @@ fi
 cd WebClient
 npm install
 cd .. dotnet restore 
+
 dotnet build -c Release -o ./artifacts
+
+echo "Running Tests"
+
+dotnet test hfa.synker.batch.test/hfa.synker.batch.test.csproj
+dotnet test hfa.tvhLibrary.test/hfa.tvhLibrary.test.csproj
