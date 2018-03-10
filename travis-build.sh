9763a490a7aab9df1cde7ad3461e14e697eae7c9
Fazzani <tunisienheni@outlook.com>
2018-03-10 14:55:23 +0100

    fix travis

--- travis-build.sh
+++ travis-build.sh
@@ -3,15 +3,23 @@
 set -evuxo
 echo "$TRAVIS_TAG"
 if [ -z "$TRAVIS_TAG" ]; then
-	echo "Isn't a Docker build";
+  echo "Isn't a Docker build";
+  exit 0
 fi
 
 if [[ "$DOCKER_BUILD" == true ]]; then
   echo "Is a Docker build";
   docker login -u=$DOCKER_USER -p=$DOCKER_PASS
-  version=${TRAVIS_TAG:latest}
+
+  # build image with github tag version
+  version=$TRAVIS_TAG
   export $version
   docker-compose build
+
+  # build image with latest tag version
+  version="latest"
+  export $version
+
   docker-compose push
   exit 0
 fi
