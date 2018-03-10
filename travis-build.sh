ca57f38dc9e42d3dd97e81705cb7cc70aa619ce7
Fazzani <tunisienheni@outlook.com>
2018-03-10 14:38:18 +0100

    fix travis docker build

--- travis-build.sh
+++ travis-build.sh
@@ -1,37 +1,18 @@
 #!/usr/bin/env bash
 
 set -evuxo
+echo "$TRAVIS_TAG"
 
-dockerImageBase="synker/broker"
-dockerImage=$dockerImageBase:linux-$arch
-echo $dockerImage
-
-if [[ "$DOCKER_BUILD" == true ]]; then
-  echo "Is Docker build";
-  cd Brokers/
-  docker build --build-arg arch=$arch -t $dockerImage -t $dockerImageBase:latest .
-  docker images
-  docker run --rm $dockerImage
+if [[ "$DOCKER_BUILD" == true && -z "$TRAVIS_TAG" ]]; then
+  echo "Is a Docker build";
   docker login -u=$DOCKER_USER -p=$DOCKER_PASS
-  docker push $dockerImage
+  version=${TRAVIS_TAG:latest}
+  export $version
+  docker-compose build
+  docker-compose push
   exit 0
 fi
 
-artifactsFolder="./artifacts"
-
-if [ -d $artifactsFolder ]; then  
-  rm -R $artifactsFolder
-fi
-
-cd WebClient
-npm install
-dotnet restore 
-dotnet build -c Release -o ./artifacts
-
-echo "Running Tests"
-
-#dotnet test hfa.synker.batch.test/hfa.synker.batch.test.csproj
-cd ../hfa.tvhLibrary.test
-dotnet restore && dotnet xunit
+echo "Isn't a Docker build";
 
 exit 0
