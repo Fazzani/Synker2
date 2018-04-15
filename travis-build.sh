cce7b7f7bd6c761b0b9de5f96087604b4b4ff441
Fazzani <tunisienheni@outlook.com>
2018-04-15 22:04:40 +0200

    build_batch

--- travis-build.sh
+++ travis-build.sh
@@ -34,19 +34,19 @@ if [[ "$DOCKER_BUILD" == true ]]; then
   
   # si le message contient build_webclient alors on build uniquement le client
   if [[ $TRAVIS_COMMIT_MESSAGE == *"build_webclient"* ]]; then
-    build_only_project $version "webclient" "WebClient/Dockerfile"
+    build_only_project "webclient" $version "WebClient/Dockerfile"
 	exit 0
   fi
 
   # si le message contient build_webapi alors on build uniquement l'api
   if [[ $TRAVIS_COMMIT_MESSAGE == *"build_webapi"* ]]; then
-    build_only_project $version "webapi" "WebApi/Dockerfile"
+    build_only_project "webapi" $version "WebApi/Dockerfile"
 	exit 0
   fi
 
   # si le message contient build_webapi alors on build uniquement l'api
   if [[ $TRAVIS_COMMIT_MESSAGE == *"build_batch"* ]]; then
-    build_only_project $version "batch" "SyncLibrary/Dockerfile"
+    build_only_project "batch" $version "SyncLibrary/Dockerfile"
 	exit 0
   fi
 
