1b9cb6a5c39d416020d1f1d54042c40d6ea88d4a
Fazzani <tunisienheni@outlook.com>
2018-06-19 18:13:25 +0200

    Update environment.prod.ts

--- SynkerClient/ClientApp/src/environments/environment.prod.ts
+++ SynkerClient/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   base_url: "/",
-  base_hub_url: "http://localhost:56800/",
-  base_api_url: "http://localhost:56800/api/v1/"
+  base_hub_url: "http://api.synker.ovh/",
+  base_api_url: "http://api.synker.ovh/"
 };
