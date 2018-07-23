0317bdf16028a4112d049562cfbaf1e6481ea908
Fazzani <tunisienheni@outlook.com>
2018-07-23 22:25:48 +0200

    fix Mixed Content

--- SynkerClient/ClientApp/src/app/components/dialogs/mediaWatch/media.watch.dialog.ts
+++ SynkerClient/ClientApp/src/app/components/dialogs/mediaWatch/media.watch.dialog.ts
@@ -26,7 +26,7 @@ export class MediaWatchDialog implements OnInit, OnDestroy {
     console.log(`playing ${this.data.url} ${this.data.displayName}`);
 
     this.player = new Clappr.Player({
-      source: this.data.url.replace('.ts', '.m3u8'),
+      source: this.data.url.replace('.ts', '.m3u8').replace('http:', ''),
       autoPlay: true,
       plugins: [PlaybackRatePlugin, ClapprStats, ChromecastPlugin, LevelSelector],
       parentId: '#player',
